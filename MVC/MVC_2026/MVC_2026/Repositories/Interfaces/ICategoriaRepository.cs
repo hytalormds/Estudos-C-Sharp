f233c994b224e0dc3dfd54e2b5d8d55852b10263
Hytalo Rodrigues Moreira de Sousa <hytalormds.contato@gmail.com>
2026-04-29 13:47:09 -0300

    Api

--- MVC/MVC_2026/MVC_2026/Repositories/Interfaces/ICategoriaRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using MVC_2026.Models;
-
-namespace MVC_2026.Repositories.Interfaces
-{
-    public interface ICategoriaRepository
-    {
-        IEnumerable<Categoria> Categorias { get; }
-    }
-}
